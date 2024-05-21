d1f792721433b9254ce2c37804bfb9326b2cb361
Guilherme Coutinho <coutinhocguilherme@gmail.com>
2024-05-21 09:41:08 -0300

    Primeiro envio

---
+++ src/main/java/coutinho/guilherme/loja/testes/CadastroDePedido.java
@@ -0,0 +1,88 @@
+package coutinho.guilherme.loja.testes;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import coutinho.guilherme.loja.dao.CategoriaDao;
+import coutinho.guilherme.loja.dao.ClienteDao;
+import coutinho.guilherme.loja.dao.PedidoDao;
+import coutinho.guilherme.loja.dao.ProdutoDao;
+import coutinho.guilherme.loja.modelo.Categoria;
+import coutinho.guilherme.loja.modelo.Cliente;
+import coutinho.guilherme.loja.modelo.ItemPedido;
+import coutinho.guilherme.loja.modelo.Pedido;
+import coutinho.guilherme.loja.modelo.Produto;
+import coutinho.guilherme.loja.util.JPAUtil;
+import coutinho.guilherme.loja.vo.RelatorioDeVendasVo;
+
+public class CadastroDePedido {
+
+	public static void main(String[] args) {
+		popularBancoDeDados();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		ClienteDao clienteDao = new ClienteDao(em);
+		
+		Produto produto = produtoDao.buscarPorId(1l);
+		Produto produto2 = produtoDao.buscarPorId(2l);
+		Produto produto3 = produtoDao.buscarPorId(3l);
+		Cliente cliente = clienteDao.buscarPorId(1l);
+		
+		em.getTransaction().begin();
+		
+		Pedido pedido = new Pedido(cliente);
+		pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+		pedido.adicionarItem(new ItemPedido(40, pedido, produto2));
+
+		Pedido pedido2 = new Pedido(cliente);
+		pedido2.adicionarItem(new ItemPedido(2, pedido2, produto3));
+		
+		PedidoDao pedidoDao = new PedidoDao(em);
+		pedidoDao.cadastrar(pedido);
+		pedidoDao.cadastrar(pedido2);
+		
+		em.getTransaction().commit();
+		
+		BigDecimal totalVendido = pedidoDao.valorTotalVendido();
+		System.out.println("VALOR TOTAL: " +totalVendido);
+		
+		
+		List<RelatorioDeVendasVo> relatorio = pedidoDao.relatorioDeVendas();
+		relatorio.forEach(System.out::println);
+	}
+	
+	private static void popularBancoDeDados() {
+		Categoria celulares = new Categoria("CELULARES");
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+		
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+		Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("8000"), videogames);
+		Produto macbook = new Produto("Macbook", "Macboo pro retina", new BigDecimal("14000"), informatica);
+		
+		Cliente cliente = new Cliente("Rodrigo", "123456");
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+		ClienteDao clienteDao = new ClienteDao(em);
+		
+		em.getTransaction().begin();
+		
+		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(videogames);
+		categoriaDao.cadastrar(informatica);
+		
+		produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(videogame);
+		produtoDao.cadastrar(macbook);
+		
+		clienteDao.cadastrar(cliente);
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
---
+++ src/main/java/coutinho/guilherme/loja/testes/PerformanceConsultas.java
@@ -0,0 +1,72 @@
+package coutinho.guilherme.loja.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import coutinho.guilherme.loja.dao.CategoriaDao;
+import coutinho.guilherme.loja.dao.ClienteDao;
+import coutinho.guilherme.loja.dao.PedidoDao;
+import coutinho.guilherme.loja.dao.ProdutoDao;
+import coutinho.guilherme.loja.modelo.Categoria;
+import coutinho.guilherme.loja.modelo.Cliente;
+import coutinho.guilherme.loja.modelo.ItemPedido;
+import coutinho.guilherme.loja.modelo.Pedido;
+import coutinho.guilherme.loja.modelo.Produto;
+import coutinho.guilherme.loja.util.JPAUtil;
+
+public class PerformanceConsultas {
+
+	public static void main(String[] args) {
+		popularBancoDeDados();
+		EntityManager em = JPAUtil.getEntityManager();
+		PedidoDao pedidoDao = new PedidoDao(em);
+		Pedido pedido = pedidoDao.buscarPedidoComCliente(1l);
+		em.close();
+		System.out.println(pedido.getCliente().getNome());
+	}
+
+	private static void popularBancoDeDados() {
+		Categoria celulares = new Categoria("CELULARES");
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+		Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("8000"), videogames);
+		Produto macbook = new Produto("Macbook", "Macboo pro retina", new BigDecimal("14000"), informatica);
+
+		Cliente cliente = new Cliente("Rodrigo", "123456");
+
+		Pedido pedido = new Pedido(cliente);
+		pedido.adicionarItem(new ItemPedido(10, pedido, celular));
+		pedido.adicionarItem(new ItemPedido(40, pedido, videogame));
+
+		Pedido pedido2 = new Pedido(cliente);
+		pedido2.adicionarItem(new ItemPedido(2, pedido, macbook));
+
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+		ClienteDao clienteDao = new ClienteDao(em);
+		PedidoDao pedidoDao = new PedidoDao(em);
+
+		em.getTransaction().begin();
+
+		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(videogames);
+		categoriaDao.cadastrar(informatica);
+
+		produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(videogame);
+		produtoDao.cadastrar(macbook);
+
+		clienteDao.cadastrar(cliente);
+
+		pedidoDao.cadastrar(pedido);
+		pedidoDao.cadastrar(pedido2);
+
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
